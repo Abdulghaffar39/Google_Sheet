651cca8833b26277eb21aea58191734ece7a4cc5
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-04-22 12:44:01 +0500

    div working

---
+++ app.js
@@ -0,0 +1,8 @@
+const SHEET_ID = "1_m1r4uqhlNMosyy5GQBg0XgqqKGCqfJWyydwnIARAxg";
+const RANGE = "A1:J";
+const API_KEY = "AIzaSyAujRd62kDheKBWAGbydc1YglJE6KDD8v8";
+
+const api =`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?axios.get_KEY}`;
+
+const div = document.getElementById(`container`);
+
